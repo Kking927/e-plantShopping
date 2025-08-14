582cd43ec09eb20998c60ad186f087c0160164dd
Kking927 <kking927@hotmail.com>
2025-08-14 18:30:28 -0500

    Update ProductList.jsx

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
-import './ProductList.css'
+import { useDispatch } from 'react-redux';  // <-- needed to dispatch actions
+import { addItem } from './CartSlice';      // <-- import the addItem action from CartSlice
+import './ProductList.css';
 import CartItem from './CartItem';
 function ProductList({ onHomeClick }) {
     const [showCart, setShowCart] = useState(false);
@@ -252,6 +254,14 @@ function ProductList({ onHomeClick }) {
         e.preventDefault();
         setShowCart(false);
     };
+    const handleAddToCart = (product) => {
+        dispatch(addItem(product)); // Dispatch the action to add the product to the cart (Redux action)
+      
+        setAddedToCart((prevState) => ({ // Update the local state to reflect that the product has been added
+          ...prevState, // Spread the previous state to retain existing entries
+          [product.name]: true, // Set the current product's name as a key with value 'true' to mark it as added
+        }));
+      };
     return (
         <div>
             <div className="navbar" style={styleObj}>
