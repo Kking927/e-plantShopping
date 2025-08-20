3f8efe863fcbca403e17471646c63d1743089c78
Katelyn King <kking927@hotmail.com>
2025-08-20 12:12:07 -0400

    Finished Task 1

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,7 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import './ProductList.css'
 import CartItem from './CartItem';
+import { useDispatch } from 'react-redux';
+import { addItem } from './CartSlice'; // adjust path if necessary
 function ProductList({ onHomeClick }) {
+    const dispatch = useDispatch();
     const [showCart, setShowCart] = useState(false);
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
     const [addedToCart, setAddedToCart] = useState({});
@@ -261,7 +264,7 @@ function ProductList({ onHomeClick }) {
           [product.name]: true, // Set the current product's name as a key with value 'true' to mark it as added
         }));
       };
-      
+
     return (
         <div>
             <div className="navbar" style={styleObj}>
