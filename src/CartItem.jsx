0c6dbebb862cb9d89254af0cae626eaeb356f225
Kking927 <kking927@hotmail.com>
2025-08-21 14:32:27 -0500

    Update CartItem.jsx

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -89,10 +89,9 @@ const CartItem = ({ onContinueShopping }) => {
           </div>
         ))}
       </div>
-      <div
-        style={{ marginTop: '20px', color: 'black' }}
-        className="total_cart_amount"
-      ></div>
+      <div className="cart-total" style={{ marginTop: '20px', fontWeight: 'bold' }}>
+        Cart Total: ${calculateTotalAmount()}
+      </div>
       <div className="continue_shopping_btn">
         <button
           className="get-started-button"
